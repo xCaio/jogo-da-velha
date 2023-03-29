8346dff698ef8bd25263052d10d93fb60cc13de7
xCaio <caiosantos1703@gmail.com>
2023-03-28 23:03:46 -0300

    Desenvolvendo Navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import menu from './menu.svg';
 
 function App() {
   return (
@@ -7,17 +8,21 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edite <code>src/App.js</code> e salve para reload.
+          {/* Edite <code>src/App.js</code> e salve para reload. */}
+          SOBRE
+          <img src={menu} className="App-menu" alt='Menu' width={"20px"}/>
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           React
-        </a>
+        </a> */}
+        
       </header>
+
     </div>
   );
 }
